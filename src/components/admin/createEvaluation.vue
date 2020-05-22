872de6cf4246e0b03db9c09ce8b414e7333b538e
Dung Bui <bmdVC013@gmail.com>
2020-05-22 16:55:32 +0700

    evaluation software development section...

--- src/components/admin/createEvaluation.vue
+++ src/components/admin/createEvaluation.vue
@@ -1,16 +1,18 @@
 <template>
   <v-content>
     <v-container fluid>
-      <v-row align="start" justify="start">
+      <v-row>
         <v-col>
           <v-card>
             <v-card-subtitle class="font-weight-bold">
               <v-icon class="mr-2">mdi-information</v-icon>
               <span>Evaluation Information</span>
             </v-card-subtitle>
+
             <v-divider class="mx-0"></v-divider>
-            <v-card-text>
-              <v-row class="evaluation-line d-flex align-center" dense>
+
+            <v-card-text class="evaluation-info">
+              <v-row class="evaluation-line" dense>
                 <v-col cols="2">
                   <p class="subtitle-1">Type of review:</p>
                 </v-col>
@@ -23,7 +25,8 @@
                   ></v-select>
                 </v-col>
               </v-row>
-              <v-row class="evaluation-line d-flex align-center" dense>
+
+              <v-row class="evaluation-line" dense>
                 <v-col cols="2">
                   <p class="subtitle-1">Name:</p>
                 </v-col>
@@ -36,10 +39,12 @@
                   ></v-select>
                 </v-col>
               </v-row>
-              <v-row class="evaluation-line d-flex align-center" dense>
+
+              <v-row class="evaluation-line" dense>
                 <v-col cols="2">
                   <p class="subtitle-1">Period of review:</p>
                 </v-col>
+
                 <v-col cols="2">
                   <v-menu
                     ref="menuRangeText"
@@ -75,19 +80,164 @@
                   </v-menu>
                 </v-col>
               </v-row>
-              <v-row class="evaluation-line d-flex align-center" dense>
-                <v-col cols="2">
-                  <p class="subtitle-1">Recurrence:</p>
-                </v-col>
-                <v-col cols="2">
+
+              <v-row dense>
+                <v-col>
                   <v-row>
-                    <v-checkbox
-                      v-model="checkRecurrence"
-                      :label="
-                        `${checkRecurrence ? 'No recurrence' : 'Recurrence'}`
-                      "
-                    ></v-checkbox>
+                    <v-col cols="2">
+                      <p class="subtitle-1">Recurrence:</p>
+                    </v-col>
+                    <v-col>
+                      <v-checkbox
+                        v-model="checkRecurrence"
+                        :label="
+                          `${checkRecurrence ? 'No recurrence' : 'Recurrence'}`
+                        "
+                        class="mt-n1"
+                      ></v-checkbox>
+                    </v-col>
                   </v-row>
+
+                  <v-row class="mt-n3" dense>
+                    <v-col cols="2"></v-col>
+                    <v-col cols="2" class="d-flex">
+                      <p class="subtitle-1 mr-4">Start:</p>
+                      <v-menu
+                        ref="menuRecur"
+                        v-model="menuRecur"
+                        :close-on-content-click="false"
+                        :return-value.sync="dateRecur"
+                        transition="scale-transition"
+                        offset-y
+                        min-width="290px"
+                      >
+                        <template v-slot:activator="{ on }">
+                          <v-text-field
+                            v-model="dateRecur"
+                            append-icon="mdi-calendar"
+                            readonly
+                            solo
+                            dense
+                            v-on="on"
+                            class="mt-n1"
+                          ></v-text-field>
+                        </template>
+                        <v-date-picker v-model="dateRecur" no-title scrollable>
+                          <v-spacer></v-spacer>
+                          <v-btn text @click="menuRecur = false">Cancel</v-btn>
+                          <v-btn text @click="$refs.menuRecur.save(dateRecur)"
+                            >OK
+                          </v-btn>
+                        </v-date-picker>
+                      </v-menu>
+                    </v-col>
+
+                    <v-col class="ml-7 mt-n3">
+                      <v-radio-group v-model="radios">
+                        <v-radio label="No end date" value="no-end"></v-radio>
+                        <v-radio value="end-after">
+                          <template v-slot:label>
+                            <div class="radio-recur-text align-center">
+                              <span>End after</span>
+                              <v-text-field
+                                label="Solo"
+                                placeholder="0"
+                                solo
+                                dense
+                                flat
+                                outlined
+                                class="number-end-after ml-3 mr-3 mt-5"
+                              ></v-text-field>
+                              <span>occurences</span>
+                            </div>
+                          </template>
+                        </v-radio>
+                        <v-radio value="end-by">
+                          <template v-slot:label>
+                            <div class="radio-recur-text align-center">
+                              <span>End by:</span>
+                              <v-menu
+                                ref="menuRecurEnd"
+                                v-model="menuRecurEnd"
+                                :close-on-content-click="false"
+                                :return-value.sync="dateRecurEnd"
+                                transition="scale-transition"
+                                offset-y
+                                min-width="290px"
+                              >
+                                <template v-slot:activator="{ on }">
+                                  <v-text-field
+                                    v-model="dateRecurEnd"
+                                    append-icon="mdi-calendar"
+                                    readonly
+                                    solo
+                                    dense
+                                    v-on="on"
+                                    class="ml-6 mt-5"
+                                  ></v-text-field>
+                                </template>
+                                <v-date-picker
+                                  v-model="dateRecurEnd"
+                                  no-title
+                                  scrollable
+                                >
+                                  <v-spacer></v-spacer>
+                                  <v-btn
+                                    text
+                                    color="primary"
+                                    @click="menuRecurEnd = false"
+                                    >Cancel
+                                  </v-btn>
+                                  <v-btn
+                                    text
+                                    @click="
+                                      $refs.menuRecurEnd.save(dateRecurEnd)
+                                    "
+                                    >OK
+                                  </v-btn>
+                                </v-date-picker>
+                              </v-menu>
+                            </div>
+                          </template>
+                        </v-radio>
+                      </v-radio-group>
+                    </v-col>
+                  </v-row>
+                </v-col>
+              </v-row>
+
+              <v-row>
+                <v-col>
+                  <div class="mb-2">
+                    <v-icon>mdi-message</v-icon>
+                    <span class="ml-2 font-italic">Templates</span>
+                  </div>
+
+                  <!-- Software Development Department -->
+                  <v-expansion-panels>
+                    <v-expansion-panel>
+                      <v-expansion-panel-header>
+                        <span class="text-center"
+                          >Software Development Department
+                        </span>
+                        <template v-slot:actions>
+                          <v-icon color="indigo" large>$expand</v-icon>
+                        </template>
+                      </v-expansion-panel-header>
+                      <v-expansion-panel-content>
+                        
+                      </v-expansion-panel-content>
+                    </v-expansion-panel>
+                    <v-expansion-panel>
+                      <v-expansion-panel-header>
+                        <span class="text-center">BO Department</span>
+                        <template v-slot:actions>
+                          <v-icon color="indigo" large>$expand</v-icon>
+                        </template>
+                      </v-expansion-panel-header>
+                    </v-expansion-panel>
+                    <v-expansion-panel-content></v-expansion-panel-content>
+                  </v-expansion-panels>
                 </v-col>
               </v-row>
             </v-card-text>
@@ -106,7 +256,11 @@ export default {
   data: () => ({
     typeOfReview: ["Performance Review", "Review 1", "Review 2", "Review 3"],
     dates: ["2019-09-10", "2019-09-20"],
+    dateRecur: new Date().toISOString().substr(0, 10),
+    dateRecurEnd: new Date().toISOString().substr(0, 10),
     menuRangeText: false,
+    menuRecur: false,
+    menuRecurEnd: false,
     checkRecurrence: true
   }),
 
@@ -119,7 +273,23 @@ export default {
 </script>
 
 <style scss>
+.evaluation-info {
+  padding-right: 5rem !important;
+  padding-left: 5rem !important;
+}
+
 .evaluation-line {
   height: 4rem;
+  display: flex;
+  text-align: start;
+}
+
+.radio-recur-text {
+  height: 3rem;
+  display: flex;
+}
+
+.number-end-after {
+  width: 6rem;
 }
 </style>
